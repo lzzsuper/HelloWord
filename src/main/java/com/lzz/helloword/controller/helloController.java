a87b525206bf8da9ff3591e7cdcf263076aaf14f
zezhou <302848395@qq.com>
2022-02-12 21:53:10 +0800

    test

---
+++ src/main/java/com/lzz/helloword/controller/helloController.java
@@ -0,0 +1,13 @@
+package com.lzz.helloword.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class helloController {
+    @RequestMapping("/index")
+    public String sayHello(){
+        return "吕泽洲的HelloWord";
+    }
+
+}
