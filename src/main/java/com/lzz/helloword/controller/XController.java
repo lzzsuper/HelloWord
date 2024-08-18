d46ab1418dc43a80a483ecc00169ca8852d43aaf
zezhou <302848395@qq.com>
2024-08-18 13:12:46 +0800

    test

--- src/main/java/com/lzz/helloword/controller/XController.java
+++ src/main/java/com/lzz/helloword/controller/XController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class helloController {
-    @RequestMapping("/index")
-    public String sayHello(){
+public class XController {
+    @RequestMapping("/X")
+    public String xPrint() {
         System.out.println("Hello");
-        return "吕泽洲的HelloWord!";
+        return "X";
 
     }
 
